ba0507d848e552e571e1742072776f9f4298fb38
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-12-21 09:20:41 +0330

    Add basics Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HomePage, Products, About, ErrorPage } from './pages'
 import logo from './logo.svg'
-import './App.css'
 
 function App() {
-  return <div>Hello world</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/products' element={<Products />}></Route>
+        <Route path='/about' element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import { HomePage } from './HomePage'
+import { About } from './About'
+import { Products } from './Products'
+import { ErrorPage } from './ErrorPage'
+
+export { HomePage, About, Products, ErrorPage }
